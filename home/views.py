586e28d3da6fa8c9a04ddbf94d944f63867365ed
Apprenant <h.mohamed.zouhair@gmail.com>
2022-11-24 16:11:11 +0100

    preparing the field 1

--- home/views.py
+++ home/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
+country_list = {
+                "France":[],
+                "Belgium":[],
+                "Netherlands":[],
+                "Uk":["Bristol","Edinburgh","London","Manchester"],
+                }
+
+
 def home_view(request):
-    return render(request, 'home/home_page.html')
+
+    context = {"country_list":country_list}
+
+    return render(request, 'home/home_page.html',context= context)
