f43f41bfdb0f536a6bf7a5194e1fc0fe47dd198d
Thomas-Dulompont <thomasdulompont.1@gmail.com>
2022-11-27 19:28:37 +0100

    Fix

--- home/views.py
+++ home/views.py
@@ -2,32 +2,18 @@ from django.shortcuts import render
 import pandas as pd
 import pickle
 
-
-country_list = {
-                "France":["Bordeaux","Lyon","Paris","Pays Basque"],
-                "Belgium":["Antwerp","Brussels","Ghent"],
-                "Netherlands":["Amsterdam","Rotterdam","The Hague"],
-                "Uk":["Bristol","Edinburgh","London","Manchester"],
-                }
-
-city_dict ={}
-
-
-for i in country_list:
-    for j in country_list[i]:
-        city_dict[j] = j
-
-
-
-
-
 def home_view(request):
-    
-    context = {"country_list":country_list}
+    country_list = {
+        "france":["bordeaux","lyon","paris","pays_Basque"],
+        "belgique":["antwerp","brussels","ghent"],
+        "netherlands":["amsterdam","Rotterdam","the_Hague"],
+        "united_Kingdom":["bristol","edinburgh","london","greater_manchester"],
+    }
+    context = {
+        "country_list" : country_list
+    }
 
     return render(request, 'home/home_page.html',context= context)
-
-
 
 def result_view(request, pays, ville):
     pickle_in = open(f'pickle/{pays}_{ville}.pkl', 'rb')
